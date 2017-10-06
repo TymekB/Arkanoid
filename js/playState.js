3392d9185afe44fb47fef9fca6cc3b3d0ddf4c9b
TymekB <baran.tymoteusz@gmail.com>
2017-10-06 17:49:20 +0200

    added assets

--- js/playState.js
+++ js/playState.js
@@ -2,7 +2,13 @@ var playState = {
 
     preload: function()
     {
-
+        game.load.image('ball', 'assets/ball.png');
+        game.load.image('paddle', 'assets/paddle.png');
+        game.load.image('brick0', 'assets/brick0.png');
+        game.load.image('brick1', 'assets/brick1.png');
+        game.load.image('brick2', 'assets/brick2.png');
+        game.load.image('brick3', 'assets/brick3.png');
+        game.load.image('brick4', 'assets/brick4.png');
     },
 
     create: function()
